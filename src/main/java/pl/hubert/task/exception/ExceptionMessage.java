02108a2cfd536db8df6f6ee56dd620f838400280
huga721 <szabahubert@gmail.com>
2024-02-17 13:09:49 +0100

    removed unused annotations

--- src/main/java/pl/hubert/task/exception/ExceptionMessage.java
+++ src/main/java/pl/hubert/task/exception/ExceptionMessage.java
@@ -1,10 +1,8 @@
 package pl.hubert.task.exception;
 
-import lombok.Builder;
 import lombok.Data;
 
 @Data
-@Builder
 public class ExceptionMessage {
     private int status;
     private String message;
--- src/main/java/pl/hubert/task/model/Repository.java
+++ src/main/java/pl/hubert/task/model/Repository.java
@@ -4,11 +4,9 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.*;
 
 import java.util.List;
 
-@Builder
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record Repository(
         Owner owner,
