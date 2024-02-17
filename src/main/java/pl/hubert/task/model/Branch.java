52ee2effdb83cad1033de6fc8827b9ba3f01fb49
huga721 <szabahubert@gmail.com>
2024-02-17 08:59:42 +0100

    refactor: changed model to records, added inside records of required fields

--- src/main/java/pl/hubert/task/model/Branch.java
+++ src/main/java/pl/hubert/task/model/Branch.java
@@ -1,26 +1,12 @@
 package pl.hubert.task.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
-import java.util.Map;
-
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Branch {
+public record Branch(
+        String name,
+        Commit commit) {
 
-    private String name;
-
-    @JsonProperty("last_commit_sha")
-    private String lastCommitSha;
-
-    @JsonProperty("commit")
-    private void setLastCommitShaJsonProperty(Map<String,String> branchInfo) {
-        this.lastCommitSha = branchInfo.get("sha");
+    public record Commit(String sha) {
     }
 }
--- src/main/java/pl/hubert/task/model/Repository.java
+++ src/main/java/pl/hubert/task/model/Repository.java
@@ -7,36 +7,21 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.*;
 
 import java.util.List;
-import java.util.Map;
 
-@Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class Repository {
+public record Repository(
+        Owner owner,
+        @JsonProperty("repository_name")
+        @JsonAlias("name")
+        String repositoryName,
+        @JsonIgnore
+        boolean fork,
+        List<Branch> branches) {
 
-    @JsonProperty("owner_login")
-    private String ownerLogin;
-
-    @JsonProperty("repository_name")
-    @JsonAlias("name")
-    private String repositoryName;
-
-    @JsonIgnore
-    private boolean fork;
-
-    private List<Branch> branches;
-
-    public Repository(Repository repository, List<Branch> branches) {
-        this.ownerLogin = repository.getOwnerLogin();
-        this.repositoryName = repository.getRepositoryName();
-        this.fork = repository.isFork();
-        this.branches = branches;
+    public Repository(Repository repo, List<Branch> branches) {
+        this(repo.owner, repo.repositoryName, repo.fork, branches);
     }
-
-    @JsonProperty("owner")
-    private void setOwnerLoginJsonProperty(Map<String,String> commit) {
-        this.ownerLogin = commit.get("login");
+    public record Owner(String login) {
     }
 }
